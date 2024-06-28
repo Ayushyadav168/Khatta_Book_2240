ef9b3826c188a81fb07bb41233be97a9212744ff
Krishcoder01 <jaink4474@gmail.com>
2024-06-28 16:23:11 +0530

    cd

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const hisaabModel = require("./models/hisaabModel")
 require('dotenv').config()
 
 
-
+app.set('views', path.join(__dirname, 'views'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.set("view engine", "ejs")
